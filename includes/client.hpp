39bd13dd647be72c0335352efd5e7e027c9341cf
remyd06 <remy.dedola@outlook.com>
2025-10-24 15:14:38 +0200

    Client class addings

--- includes/client.hpp
+++ includes/client.hpp
@@ -6,7 +6,7 @@
 /*   By: rdedola <rdedola@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 14:17:58 by rdedola           #+#    #+#             */
-/*   Updated: 2025/10/24 14:48:23 by rdedola          ###   ########.fr       */
+/*   Updated: 2025/10/24 15:14:26 by rdedola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,21 @@
 class client
 {
 	private:
-	
 		std::string _nick;
-		bool _pass;
 		std::string _user;
+		bool 		_pass;
+		int			_clientsSocket;
+		
 	public:
-		client();
+		client(int fd) : _nick(""), _user(""), _pass(0), _clientsSocket(fd) {}
 		~client();
+
+		std::string	getNick() {return (_nick);}
+		std::string getUser() {return (_user);}
+		bool		getPass() {return (_pass);}
+		int			getClientSocket() {return (_clientsSocket);}
+
+		void	setNick(const std::string &nick) {_nick = nick;}
+		void	setUser(const std::string &user) {_user = user;}
+		void	setPass(bool pass) {_pass = pass;}
 };

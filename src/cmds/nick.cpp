19da07e9ca5e25ad1adf87d1d929786b12acbb2a
remyd06 <remy.dedola@outlook.com>
2025-10-29 15:02:54 +0100

    nick.cpp

--- src/cmds/nick.cpp
+++ src/cmds/nick.cpp
@@ -11,14 +11,29 @@ void IRCServ::CMDnick(unsigned int clientindex, std::string &buffer)
 	for (unsigned int index = 0; buffer[index] == ' '; ++index)
 		buffer.erase(0, 1);
 	const std::string tmp = buffer.substr(0, buffer.find(' '));
-	for (unsigned int i = 0; i < _clients.size(); ++i)
+	if (tmp.length() > 9 && !isalpha(tmp[0]))
 	{
-		if (tmp == _clients[clientindex]->getNick())
+		_clients[clientindex]->sendMessage(ERR_ERRONEUSNICKNAME(tmp));
+		return ;
+	}
+	for (unsigned int i = 1; i < tmp.length(); ++i)
+	{
+		if (isalnum(tmp[i]) || tmp[i] == '-' || tmp[i] == '[' || tmp[i] == ']'
+			|| tmp[i] == '\\' || tmp[i] == '`' || tmp[i] == '^' || tmp[i] == '{' || tmp[i] == '}')
+			break ;
+		else
 		{
-			
-			return;
+			_clients[clientindex]->sendMessage(ERR_ERRONEUSNICKNAME(tmp));
+			return ;
 		}
 	}
-
+	for (unsigned int i = 0; i < _clients.size(); ++i)
+	{
+		if (tmp == _clients[i]->getNick())
+		{
+			_clients[clientindex]->sendMessage(ERR_NICKNAMEINUSE(tmp));
+			return ;
+		}
+	}
 	_clients[clientindex]->setNick(tmp);
 }

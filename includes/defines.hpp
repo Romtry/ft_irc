0df74cf0aaf5c15b4a2622aab4cb37dbc3230c9c
Romtry <romaindogcn@gmail.com>
2026-02-06 13:03:09 +0100

    more defines

--- includes/defines.hpp
+++ includes/defines.hpp
@@ -53,6 +53,27 @@
 # define RAW_PRIVMSG(sender, hostname, receiver, message) \
 	":" + sender + "!~" + sender + "@" + hostname + " PRIVMSG " + receiver + " " + message + "\r\n"
 
+# define RPL_NOTOPIC(nickname, channel) ":ft_irc.nanachi 331 " + nickname + " " + channel + " :No topic is set\r\n"
+
+# define RPL_TOPIC(nickname, channel, topic) \
+":ft_irc.nanachi 332 " + nickname + " " + channel + " :" + topic + "\r\n"
+
+# define RAW_TOPIC(nickname, username, hostname, channel, topic) \
+":" + nickname + "!~" + username + "@" + hostname + " TOPIC " + channel + " :" + topic + "\r\n"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -67,3 +88,31 @@
 
 # define RAW_QUIT(nickname, username, hostname, message) \
 ":" + nickname + "!~" + username + "@" + hostname + " QUIT :" + message + "\r\n"
+
+# define RAW_NICKNAME(oldNick, newNick, username, hostname)	\
+":" + oldNick + "!" + username + "@" + hostname + " NICK :" + newNick + "\r\n"
+
+# define RPL_NAMREPLY(target, channel, names) \
+":ft_irc.nanachi 353 " + target + " = " + channel + " :" + names + "\r\n"
+
+# define RPL_ENDOFNAMES(target, channel) \
+":ft_irc.nanachi 366 " + target + " " + channel + " :End of /NAMES list.\r\n"
+
+# define RAW_MODE_ADDOP(sender, username, hostname, channel, target) \
+(":" + sender + "!~" + username + "@" + hostname + " MODE " + channel + " +o " + target + "\r\n")
+
+# define RAW_MODE_REMOP(sender, username, hostname, channel, target) \
+(":" + sender + "!~" + username + "@" + hostname + " MODE " + channel + " -o " + target + "\r\n")
+
+# define RAW_MODE(nickname, username, hostname, channel, mode, arg) \
+":" + nickname + "!~" + username + "@" + hostname + " MODE " + channel + " " + mode + (arg.empty() ? "" : " " + arg) + "\r\n"
+
+
+# define RPL_INVITING(inviter, invited, channel) \
+":ft_irc.nanachi 341 " + std::string(inviter) + " " + std::string(invited) + " " + std::string(channel) + "\r\n"
+
+# define INVITE_MESSAGE(inviter, invited, channel) \
+":" + std::string(inviter) + "!~" + inviter + "@host INVITE " + std::string(invited) + " :" + std::string(channel) + "\r\n"
+
+# define RPL_KICK(kicker, target, channel, comment) \
+":" + std::string(kicker) + "!~" + std::string(kicker) + "@host KICK " + std::string(channel) + " " + std::string(target) + " :" + std::string(comment) + "\r\n"

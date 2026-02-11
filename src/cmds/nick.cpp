cc224b3d16a94486509fd6fba06115266ce9453f
Romtry <romaindogcn@gmail.com>
2026-02-11 15:12:24 +0100

    nick changing handle in hexchat

--- src/cmds/nick.cpp
+++ src/cmds/nick.cpp
@@ -15,28 +15,23 @@
 
 void IRCServ::CMDnick(Client *client, const std::string &buffer) const
 {
-	if (!client->getNick().empty())
-	{
-		client->sendMessage(ERR_ALREADYREGISTRED);
-		return;
-	}
 	const std::string tmp = buffer.substr(0, buffer.find(' '));
+	// std::cout << "" << std::endl;
 	if (tmp.length() > 9 && !isalpha(tmp[0]))
 	{
 		client->sendMessage(ERR_ERRONEUSNICKNAME(tmp));
 		return ;
 	}
+	// std::cout << "BUFFER OK" << std::endl;
 	for (unsigned int i = 1; i < tmp.length(); ++i)
 	{
 		if (isalnum(tmp[i]) || tmp[i] == '-' || tmp[i] == '[' || tmp[i] == ']'
 			|| tmp[i] == '\\' || tmp[i] == '`' || tmp[i] == '^' || tmp[i] == '{' || tmp[i] == '}')
 			continue ;
-		else
-		{
-			client->sendMessage(ERR_ERRONEUSNICKNAME(tmp));
-			return ;
-		}
+		client->sendMessage(ERR_ERRONEUSNICKNAME(tmp));
+		return ;
 	}
+	// std::cout << "VALID CHAR" << std::endl;
 	for (unsigned int i = 0; i < _clients.size(); ++i)
 	{
 		if (tmp == _clients[i]->getNick())
@@ -45,6 +40,32 @@ void IRCServ::CMDnick(Client *client, const std::string &buffer) const
 			return ;
 		}
 	}
+	// std::cout << "BEFOR ALREADY VERIF" << std::endl;
+	std::vector<Client *> already_send;
+	for (unsigned int i = 0; i < client->getChannels().size(); ++i)
+	{
+		for (unsigned int j = 0; j < client->getChannels()[i]->getClients().size(); ++j)
+		{
+			bool already = false;
+			for (unsigned int k = 0; k < already_send.size(); ++k)
+			{
+				if (already_send[k] == client->getChannels()[i]->getClients()[j])
+				{
+					already = true;
+					break;
+				}
+			}
+			if (already == true)
+			{
+				already = false;
+				continue;
+			}
+			client->getChannels()[i]->getClients()[j]->sendMessage(RAW_NICKNAME(client->getNick(), tmp, client->getUser(), "nanachi"));
+			already_send.push_back(client->getChannels()[i]->getClients()[j]);
+		}
+	}
+	std::cout << "NICK CHANGED" << std::endl;
+	client->sendMessage(RAW_NICKNAME(client->getNick(), tmp, client->getUser(), "nanachi"));
 	client->setNick(tmp);
 	if (client->getPass() && !client->getUser().empty())
 	{

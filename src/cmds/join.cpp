a1050ab2b7d29e9f9243526e57fa37202605623e
Romtry <romaindogcn@gmail.com>
2026-02-25 15:15:38 +0100

    WAYMORE clear join

--- src/cmds/join.cpp
+++ src/cmds/join.cpp
@@ -13,6 +13,58 @@
 #include "../../includes/Channel.hpp"
 #include "../../includes/IRCServ.hpp"
 
+bool	limiteCheck(const Channel *chan)
+{
+	if ((chan->getLimite() != 0 && chan->getLimite() <= chan->getClients().size()))
+	{
+		return (false);
+	}
+	return	(true);
+}
+
+bool	inviteCheck(Channel *chan, const Client *client)
+{
+	if (!chan->getInvite_only())
+		return (true);
+	for (unsigned int i = 0; i < chan->getGuests().size(); ++i)
+	{
+		if (chan->getGuests()[i] == client)
+			return (true);
+	}
+	return (false);
+}
+
+void	joining(Channel *chan, Client *client)
+{
+	client->addChannel(chan);
+	chan->addMember(client);
+	for (unsigned int i = 0; i < chan->getClients().size(); ++i)
+	{
+		if (chan->getClients()[i] == client)
+			continue;
+		chan->getClients()[i]->sendMessage(RAW_JOIN(client->getNick(), client->getUser(), "nanachi", chan->getChanName()));
+	}
+	client->sendMessage(RAW_JOIN(client->getNick(), client->getUser(), "nanachi", chan->getChanName()));
+}
+
+void	actuNamesList(const Channel *chan, const Client *client)
+{
+	std::string names;
+	if (chan->getOperator(chan->getClients()[0]))
+		names += "@" + chan->getClients()[0]->getNick();
+	else
+		names += chan->getClients()[0]->getNick();
+	for (unsigned int i = 1; i < chan->getClients().size(); ++i)
+	{
+		if (chan->getOperator(chan->getClients()[i]))
+			names += " @" + chan->getClients()[i]->getNick();
+		else
+			names += " " + chan->getClients()[i]->getNick();
+	}
+	client->sendMessage(RPL_NAMREPLY(client->getNick(), chan->getChanName(), names));
+	client->sendMessage(RPL_ENDOFNAMES(client->getNick(), chan->getChanName()));
+}
+
 void	IRCServ::CMDjoin(Client *client, std::string &buffer)
 {
 	if (!client->getIsRegister())
@@ -20,124 +72,44 @@ void	IRCServ::CMDjoin(Client *client, std::string &buffer)
 		client->sendMessage(ERR_NOTREGISTERED);
 		return;
 	}
-	std::string channelName = buffer.substr(0, buffer.find_first_of(' '));
+	const std::string channelName = buffer.substr(0, buffer.find_first_of(' '));
+	nextArg(buffer);
 	if (channelName[0] != '#' || !channelName[1])
 		return;
-	buffer.erase(0, buffer.find_first_of(' '));
-	buffer.erase(0, buffer.find_first_not_of(' '));
-	std::string pwd = buffer.substr(0, buffer.find_first_of(' '));
-	buffer.erase(0, buffer.find_first_of(' '));
-	buffer.erase(0, buffer.find_first_not_of(' '));
+	const std::string pwd = buffer.substr(0, buffer.find_first_of(' '));
+	nextArg(buffer);
 	for (unsigned int i = 0; i < client->getChannels().size(); ++i)
 	{
 		if (client->getChannels()[i]->getChanName() == channelName)
-		{
 			 return;
-		}
 	}
-	// std::cout << "channel size: " << _channels.size() << std::endl;
-	if (_channels.size() > 0)
-		// std::cout << "channel[0]: " << _channels[0]->getChanName() << std::endl;
-	for (unsigned int i = 0; i < _channels.size(); ++i)
+	Channel *chan = findChannel(channelName);
+	if (chan != NULL)
 	{
-		if (_channels[i]->getChanName() == channelName)
+		if (!limiteCheck(chan))
 		{
-			if ((_channels[i]->getLimite() == 0 || _channels[i]->getLimite() > _channels[i]->getClients().size()))
-			{
-				if (!_channels[i]->getInvite_only())
-				{
-					if (_channels[i]->getPassword().empty() || pwd == _channels[i]->getPassword())
-					{
-						client->addChannel(_channels[i]);
-						_channels[i]->addMember(client);
-						for (unsigned int k = 0; k < _channels[i]->getClients().size(); ++k)
-						{
-							if (_channels[i]->getClients()[k] == client)
-								continue;
-							_channels[i]->getClients()[k]->sendMessage(RAW_JOIN(client->getNick(), client->getUser(), "nanachi", channelName));
-						}
-						client->sendMessage(RAW_JOIN(client->getNick(), client->getUser(), "nanachi", channelName));
-						std::string names;
-						for (unsigned int k = 0; k < _channels[i]->getClients().size(); ++k)
-						{
-							if (k == 0)
-							{
-								if (_channels[i]->getOperator(_channels[i]->getClients()[k]))
-									names += "@" + _channels[i]->getClients()[k]->getNick();
-								else
-									names += _channels[i]->getClients()[k]->getNick();
-							}
-							else
-							{
-								if (_channels[i]->getOperator(_channels[i]->getClients()[k]))
-									names += " @" + _channels[i]->getClients()[k]->getNick();
-								else
-									names += " " + _channels[i]->getClients()[k]->getNick();
-							}
-						}
-						// std::cout << "names: " << names << std::endl;
-						client->sendMessage(RPL_NAMREPLY(client->getNick(), _channels[i]->getChanName(), names));
-						client->sendMessage(RPL_ENDOFNAMES(client->getNick(), _channels[i]->getChanName()));
-						if (!_channels[i]->getTopic().empty())
-							client->sendMessage(RAW_TOPIC(client->getNick(), client->getUser(), "nanachi", client->getChannels()[i]->getChanName(), _channels[i]->getTopic()));
-						return ;
-					}
-				}
-				for (unsigned int j = 0; j < _channels[i]->getGuests().size(); ++j)
-				{
-					if (_channels[i]->getGuests()[j] == client)
-					{
-						if (_channels[i]->getPassword().empty() || pwd == _channels[i]->getPassword())
-						{
-							client->addChannel(_channels[i]);
-							_channels[i]->addMember(client);
-							for (unsigned int k = 0; k < _channels[i]->getClients().size(); ++k)
-							{
-								if (_channels[i]->getClients()[k] == client)
-									continue;
-								_channels[i]->getClients()[k]->sendMessage(RAW_JOIN(client->getNick(), client->getUser(), "nanachi", channelName));
-							}
-							client->sendMessage(RAW_JOIN(client->getNick(), client->getUser(), "nanachi", channelName));
-							std::string names;
-							for (unsigned int k = 0; k < _channels[i]->getClients().size(); ++k)
-							{
-								if (k == 0)
-								{
-									if (_channels[i]->getOperator(_channels[i]->getClients()[k]))
-										names += "@" + _channels[i]->getClients()[k]->getNick();
-									else
-										names += _channels[i]->getClients()[k]->getNick();
-								}
-								else
-								{
-									if (_channels[i]->getOperator(_channels[i]->getClients()[k]))
-										names += " @" + _channels[i]->getClients()[k]->getNick();
-									else
-										names += " " + _channels[i]->getClients()[k]->getNick();
-								}
-							}
-							// std::cout << "names: " << names << std::endl;
-							client->sendMessage(RPL_NAMREPLY(client->getNick(), _channels[i]->getChanName(), names));
-							client->sendMessage(RPL_ENDOFNAMES(client->getNick(), _channels[i]->getChanName()));
-							_channels[i]->getGuests().erase(_channels[i]->getGuests().begin() + j);
-							if (!_channels[i]->getTopic().empty())
-								client->sendMessage(RAW_TOPIC(client->getNick(), client->getUser(), "nanachi", client->getChannels()[i]->getChanName(), _channels[i]->getTopic()));
-							return ;
-						}
-					}
-					client->sendMessage(ERR_INVITEONLYCHAN(client->getNick(), _channels[i]->getChanName()));
-				}
-			}
+			client->sendMessage(ERR_CHANNELISFULL(channelName));
 			return;
 		}
+		if (!inviteCheck(chan, client))
+		{
+			client->sendMessage(ERR_INVITEONLYCHAN(chan->getChanName()));
+			return;
+		}
+		if (!chan->getPassword().empty() && pwd != chan->getPassword())
+		{
+			client->sendMessage(ERR_PASSWDMISMATCH);
+			return;
+		}
+		joining(chan, client);
+		actuNamesList(chan, client);
+		if (!chan->getTopic().empty())
+			client->sendMessage(RAW_TOPIC(client->getNick(), client->getUser(), "nanachi", chan->getChanName(), chan->getTopic()));
+		return;
 	}
-	Channel *tmp = new Channel(client, channelName);
-	tmp->addMember(client);
-	client->addChannel(tmp);
-	client->sendMessage(RAW_JOIN(client->getNick(), client->getUser(), "nanachi", channelName));
-	std::string name = "@" + client->getNick();
-	// std::cout << "name: " << name << std::endl;
-	client->sendMessage(RPL_NAMREPLY(client->getNick(), tmp->getChanName(), name));
-	client->sendMessage(RPL_ENDOFNAMES(client->getNick(), tmp->getChanName()));
-	_channels.push_back(tmp);
+	Channel *newChan = new Channel(client, channelName);
+	newChan->addMember(client);
+	joining(newChan, client);
+	actuNamesList(newChan, client);
+	_channels.push_back(newChan);
 }
